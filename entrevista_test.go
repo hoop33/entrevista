2c8c46509299131da55caeda10b54e4194f4bdf4
Rob Warner <rob.warner@availity.com>
2015-08-04 13:43:38 -0400

    Fix example

--- entrevista_test.go
+++ entrevista_test.go
@@ -8,20 +8,29 @@ import (
 
 func Example() {
 	interview := entrevista.NewInterview()
+	interview.ReadAnswer = func(question *entrevista.Question) (string, error) {
+		return question.Key, nil
+	}
 	interview.Questions = []entrevista.Question{
 		{
+			Key:      "name",
 			Text:     "Enter your name",
 			Required: true,
 		},
 		{
+			Key:           "email",
 			Text:          "Enter your email address",
 			DefaultAnswer: "john.doe@example.com",
 		},
 	}
 	answers, err := interview.Run()
-	if err != nil {
-		for _, answer := range answers {
-			fmt.Println(answer)
+
+	if err == nil {
+		for key, answer := range answers {
+			fmt.Print(key, ":", answer, ";")
 		}
+	} else {
+		fmt.Print(err.Error())
 	}
+	// Output: Enter your name: Enter your email address (john.doe@example.com): name:name;email:email;
 }
